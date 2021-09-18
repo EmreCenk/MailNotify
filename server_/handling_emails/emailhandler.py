4b9941680f6560a1226da44d55e112b5fb0d165d
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2021-09-18 14:23:54 -0400

    Implemented Email

---
+++ server_/handling_emails/emailhandler.py
@@ -0,0 +1,71 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from datetime import date
+tod = date.today() #date
+today=str(tod)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+exampleUsername = os.environ["USERNAME123123123"]
+examplePassword = os.environ["PASSWORD123123123"]
+
+class email:
+  def send(email):
+    
+    
+    
+    # print("USERNAME:",exampleUsername)
+    # print("PASSWORD", examplePassword)
+
+    # Define these once; use them twice!
+    strFrom = "OfficialMailBoxNotifier"
+    strTo = email
+
+    # Create the root message and fill in the from, to, and subject headers
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'Mailbox alert'
+    msgRoot['From'] = strFrom
+    msgRoot['To'] = strTo
+    msgRoot.preamble = 'Mailbox has just been triggered at '+today+'! Here are the details:'
+
+
+    msgAlternative = MIMEMultipart('')
+    msgRoot.attach(msgAlternative)
+
+    msgText = MIMEText(today)
+    msgAlternative.attach(msgText)
+
+   
+    msgText = MIMEText('<b>The Mailbox Notifier has been triggered!</b><br>Here is the image of the delievery inside the mailbox: <br><img src="cid:image1" style="width:700px;height:500px" alt="Inside mailbox"><br><script>const b = new Date(); document.getElementById("date").innerHTML = b;</script>', 'html')
+    msgAlternative.attach(msgText)
+
+    # This example assumes the image is in the current directory
+
+    file = open("server_/test_image.png", "rb")
+    msgImage = MIMEImage(file.read())
+    file.close()
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', '<image1>')
+    msgRoot.attach(msgImage)
+
+    # Send the email (this example assumes SMTP authentication is required)
+    import smtplib
+    smtp = smtplib.SMTP("smtp.gmail.com:587")
+                                                      #contacts the server
+    smtp.connect("smtp.gmail.com:587")
+
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+
+    smtp.login(exampleUsername, examplePassword)
+
+    smtp.sendmail(strFrom, strTo, msgRoot.as_string())  #logins
+
+    smtp.quit()
+    print('sent successfully')
+
+if __name__ == "__main__":
+    email.send("ridwan3968@gmail.com") #Sends test email
