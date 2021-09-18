912f0914d3861f96d1cbeddcb01d4156680b02a9
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2021-09-18 14:23:29 -0400

    Update twilosms.py

--- server_/sms/twilosms.py
+++ server_/sms/twilosms.py
@@ -6,13 +6,13 @@ account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 send = Client(account_sid, auth_token)
 class sms:
-
-    @staticmethod
-    def send(phonenumber):
+    def send(phonenumber,image_url):
         send.messages.create(from_=os.environ.get('TWILIO_PHONE_NUMBER'),
                       to=phonenumber,
+                      media_url=[image_url],
                       body='Mailbox has been opened. Check your mailbox for new items now!')
+        print("sent successfully")
 
 
 if __name__ == '__main__':
-    sms.send('+15197215818')
+    sms.send('+12265074010',"https://i.insider.com/61135525ad63f30019501966?width=700")
