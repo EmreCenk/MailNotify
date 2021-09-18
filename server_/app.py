cd32f962ac323e6898ec1a26da9c0ca02f5e3d9f
Emre Cenk <emrecenk9@gmail.com>
2021-09-18 12:13:07 -0400

    wrapped saving an image inside a function

--- server_/app.py
+++ server_/app.py
@@ -6,6 +6,32 @@ def valid_serial(serial_number):
     #TODO: Check if the serial number is registered in the serial number database
     return True
 
+def save_image():
+    serial_num = request.values["serial"]
+
+    if not valid_serial(serial_num):
+        return ""
+
+    image = request.files["image"]
+    print("image name:", image)
+
+    proper_date = str(datetime.now()).replace(".", "-").replace(":", "-")
+
+    path_to_save = os.path.join(app.config["IMAGE_UPLOADS"],
+                                str(serial_num),
+                                )
+
+    if not os.path.exists(path_to_save):
+        print("creating:", path_to_save)
+        os.mkdir(path_to_save)
+        print("yuh")
+
+    original = os.getcwd()
+    os.chdir(path_to_save)
+    # path_to_save = os.path.join(path_to_save, str(datetime.now())+".png")
+    image.save(proper_date + ".png")
+    os.chdir(original)
+
 app = Flask(__name__, template_folder="", static_folder="")
 app.config["IMAGE_UPLOADS"] = "uploads"
 
@@ -15,30 +41,7 @@ def upload():
 
     if request.method == "POST":
 
-        serial_num = request.values["serial"]
-
-        if not valid_serial(serial_num):
-            return ""
-
-        image = request.files["image"]
-        print("image name:", image)
-
-        proper_date = str(datetime.now()).replace(".","-").replace(":", "-")
-
-        path_to_save = os.path.join(app.config["IMAGE_UPLOADS"],
-                                    str(serial_num),
-                                    )
-
-        if not os.path.exists(path_to_save):
-            print("creating:", path_to_save)
-            os.mkdir(path_to_save)
-            print("yuh")
-
-        original = os.getcwd()
-        os.chdir(path_to_save)
-        # path_to_save = os.path.join(path_to_save, str(datetime.now())+".png")
-        image.save(proper_date + ".png")
-        os.chdir(original)
+        save_image()
         return ""
     else:
         return redirect(request.url)
