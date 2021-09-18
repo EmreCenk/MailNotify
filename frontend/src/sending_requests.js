8431c21790c7397db850d221ba7bdcc513e39eb2
Emre Cenk <emrecenk9@gmail.com>
2021-09-18 16:31:30 -0400

    implemented ate string parsing

--- frontend/src/sending_requests.js
+++ frontend/src/sending_requests.js
@@ -1,21 +1,32 @@
 import axios from 'axios'
 
-
+function parse_date_string(date_string){
+    date_string = date_string.split(" ");
+    date_string[0] = date_string[0].split("-")
+    date_string[1] = date_string[1].split("-")
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    console.log(date_string);
+    return String(monthNames[parseInt(date_string[0][1])-1] +
+    " " + date_string[0][2] + ", " + date_string[0][0] + " | " + date_string[1][0] + ":" + date_string[1][1] )
+}
 async function get_user_information(email_of_user){
     /*
     email_of_user [string]: email of the user you would like to receive information on
 
-    Returns: an object in the following format:
+    Returns: an array of objects. The return value is in the following format:
+    
+    [
     {
-        '694394747906926353': {
-        date_string: '2021-09-18 12-34-18-296168',
-        image_url: 'uploads/emrecenk9/2021-09-18 12-33-22-693446.png',      weight_string: '12 kg'
-        },
-        '694401203183298321': {
-        date_string: '2021-09-18 13-07-08-145157',
-        image_url: 'uploads/emrecenk9/2021-09-18 12-34-18-296168.png',      weight_string: '12 kg'
-        }
+        date_string: 'September 18, 2021 | 12:34',
+        image_url: 'uploads/emrecenk9/2021-09-18 12-33-22-693446.png',
+        weight_string: '12 kg'
+    },
+    {
+        date_string: 'September 18, 2021 | 13:07',
+        image_url: 'uploads/emrecenk9/2021-09-18 12-34-18-296168.png',
+        weight_string: '12 kg'
     }
+    ]
 
     each entry is an id (you don't have to worry about what they are. )
 
@@ -28,15 +39,17 @@ async function get_user_information(email_of_user){
     
     const response = await axios.get(url, {params: parameters});
     let final_list = [];
-
+    let some_var;
     for (const id_ in response.data) {
-        final_list.push(response.data[id_]);
+        some_var = response.data[id_];
+        console.log("HERE", some_var.date_string, parse_date_string(some_var.date_string));
+        some_var.date_string = String(parse_date_string(some_var.date_string));
+        final_list.push(some_var);
       }
-    // console.log(typeof(response.data));
-    console.log(response.data);
     console.log(final_list);
     
     return final_list;
 }
 
 get_user_information("emrecenk9@gmail.com")
+// console.log(parse_date_string('2021-09-18 13-07-08-145157',))
