0a53fe264dad46a177bc4b84ad29ea491878377b
Emre Cenk <emrecenk9@gmail.com>
2021-09-18 11:59:03 -0400

    function for creating tables for clients

--- server_/cockroach_db/cockroach_db.py
+++ server_/cockroach_db/cockroach_db.py
@@ -6,20 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def check_if_table_exists(conn, table_name):
-
+    table_name = table_name.replace("@gmail.com", "")
     cur = conn.cursor()
     cur.execute("select * from information_schema.tables where table_name=%s", (table_name,))
     result = bool(cur.rowcount)
     cur.close()
+
     return result
 
+def create_client_table(conn, client_email, ):
+    client_email = client_email.replace("@gmail.com", "")
+    cur = conn.cursor()
+    cur.execute(f"CREATE TABLE {client_email} (id SERIAL PRIMARY KEY, date_string VARCHAR, weight VARCHAR);")
+    cur.close()
+    conn.commit()
 
-connection_string = os.environ["CONNECTION_STRING_TO_COCKROACHDB"]
-conn = psycopg2.connect(connection_string)
-doesit = check_if_table_exists(conn, "student")
-print(doesit)
+if __name__ == '__main__':
 
-conn.close()
+    connection_string = os.environ["CONNECTION_STRING_TO_COCKROACHDB"]
+    conn = psycopg2.connect(connection_string)
+    print(check_if_table_exists(conn, "emrecenk9@gmail.com"))
+    create_client_table(conn, "emrecenk9@gmail.com")
+    print(check_if_table_exists(conn, "emrecenk9@gmail.com"))
+    conn.close()
 #
 # #creating a cursor
 # cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
