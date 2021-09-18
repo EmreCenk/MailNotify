2853e7726abb395ef2b5c3a5eb405f8c0dc98e1b
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2021-09-18 13:41:34 -0400

    Implemented SMS

---
+++ server_/sms/twilosms.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from twilio.rest import Client
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+send = Client(account_sid, auth_token)
+class sms:
+    def send(phonenumber):
+        send.messages.create(from_=os.environ.get('TWILIO_PHONE_NUMBER'),
+                      to=phonenumber,
+                      body='Mailbox has been opened. Check your mailbox for new items now!')
+
+
+if __name__ == '__main__':
+    sms.send('+15197215818')
